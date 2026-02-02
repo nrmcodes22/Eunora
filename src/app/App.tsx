63170272c92d1169daabf3a5424eee6f25761da6
Nupur <nrmcodes22@gmail.com>
2026-02-02 21:58:33 +0530

    deploment

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,11 @@
+import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Navigation } from '@/app/components/Navigation';
-import { Footer } from '@/app/components/Footer';
-import { HomePage } from '@/app/components/pages/HomePage';
-import { SolutionsPage } from '@/app/components/pages/SolutionsPage';
-import { TrainingPage } from '@/app/components/pages/TrainingPage';
-import { AboutContactPage } from '@/app/components/pages/AboutContactPage';
+import { Navigation } from '../app/components/Navigation';
+import { Footer } from '../app/components/Footer';
+import { HomePage } from '../app/components/pages/HomePage';
+import { SolutionsPage } from '../app/components/pages/SolutionsPage';
+import { TrainingPage } from '../app/components/pages/TrainingPage';
+import { AboutContactPage } from '../app/components/pages/AboutContactPage';
 
 export default function App() {
   return (
